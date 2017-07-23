72189294770a4a1efd93f174477861c3c020db43
Alex Patel <patelalex02@gmail.com>
2017-07-22 22:56:04 -0400

    add machine image module boilerplate

---
+++ guppypy/machine_image.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Module Docstring
+"""
+
+
+def main():
+    """ Main entry point of the app """
+
+    print "hello world"
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+        main()
