3df48b19bff9316869b324357c0158daac90b60f
Alex Patel <patelalex02@gmail.com>
2017-07-25 00:04:03 -0400

    define register stack restore order as SMT problem

--- guppypy/stack_restore.py
+++ guppypy/stack_restore.py
@@ -1,56 +1,46 @@
-# This is the tutorial example of pySMT.
-#
-# This example shows how to:
-# 1. Deal with Theory atoms
-# 2. Specify a solver in the shortcuts (get_model, is_sat etc.)
-# 3. Obtain an print a model
-#
-#
-# The goal of the puzzle is to assign a value from 1 to 10 to each letter s.t.
-#    H+E+L+L+O = W+O+R+L+D = 25
-#
-from pysmt.shortcuts import Symbol, And, GE, LT, Plus, Equals, Int, get_model
+#!/usr/env/bin python
+
+"""
+Use model checking to synthesize X86 assembly to restore user stack after
+system call in BarrelfishOS.
+"""
+
 from pysmt.typing import INT
+from pysmt.shortcuts import Symbol, Int, And, GE, LE, GT, get_model
 
-registers = {
-    'RIP': 	'%rcx',		# user-space RIP
-    'RFLAGS': 	'%r11',		# user-space RFLAGS
-    'RSP': 	'%rsp',		# user-space stack pointer
-    11: 	'%rbx',		# arg11
-    10: 	'%rbp',		# arg10
-    9: 		'%rax',		# arg9
-    8: 		'%r15',		# arg8
-    7: 		'%r14',		# arg7
-    6: 		'%r13',		# arg6
-    5: 		'%r12',		# arg5 
-    4: 		'%r9',		# arg4
-    3: 		'%r8',		# arg3
-    2: 		'%r10',		# arg2
-    '5f_src': 	'%r11',		# source register of 5th function arg
-    '5f_dest': 	'%r8',		# dest register of 5th function arg	
-    '6f_src': 	'%rcx',		# source register of 6th function arg
-    '6f_dest': 	'%r9',		# dest register of 6th function arg	
-    '4f_src': 	'%rsp',		# source register of 4th function arg
-    '4f_dest': 	'%rcx',		# dest register of 4th function arg	
-}
+from register import Register as REG
 
-hello = [Symbol(s, INT) for s in 'hello']
-world = [Symbol(s, INT) for s in 'world']
-letters = set(hello+world)
-domains = And([And(GE(l, Int(1)),
-                   LT(l, Int(10))) for l in letters])
 
-sum_hello = Plus(hello) # n-ary operators can take lists
-sum_world = Plus(world) # as arguments
-problem = And(Equals(sum_hello, sum_world),
-              Equals(sum_hello, Int(25)))
-formula = And(domains, problem)
+registers = dict(map(lambda r: (r, Symbol(r, INT)), [
+    '%r10', '%r11', '%r12', '%r13', '%r14', '%r15', '%r8', '%r9', '%rax', '%rbp',
+    '%rbx', '%rcx'
+]))
 
-print('Serialization of the formula:')
-print(formula)
+stack = range(2, 14)
+low, high = min(stack), max(stack)
 
-model = get_model(formula)
+def is_valid_stack_arg(reg):
+    return And(LE(reg, Int(max(stack))), GE(reg, Int(min(stack)))) 
+
+domain = [
+    And(is_valid_stack_arg(reg) for reg in registers.values()),
+    GT(registers['%rcx'], registers['%r11']), 
+    GT(registers['%r11'], registers['%rbx']),
+    GT(registers['%rbx'], registers['%rbp']),
+    GT(registers['%rbp'], registers['%rax']),
+    GT(registers['%rax'], registers['%r15']),
+    GT(registers['%rax'], registers['%r15']),
+    GT(registers['%r15'], registers['%r14']),
+    GT(registers['%r14'], registers['%r13']),
+    GT(registers['%r13'], registers['%r12']),
+    GT(registers['%r12'], registers['%r9']),
+    GT(registers['%r9'], registers['%r8']),
+    GT(registers['%r8'], registers['%r10']),
+]
+
+problem = And(domain) 
+model = get_model(problem)
 if model:
-    print(model)
+    print model
 else:
-    print('No solution found')
+    print 'No solutions found.'
