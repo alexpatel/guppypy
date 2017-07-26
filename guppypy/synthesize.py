f998a79866ac758d36143c9dbb836e2a2cf048e8
Alex Patel <patelalex02@gmail.com>
2017-07-25 21:23:50 -0400

    write CLI and barrelfish patcher/tester

--- guppypy/synthesize.py
+++ guppypy/synthesize.py
@@ -1,52 +1,76 @@
 #!/usr/env/bin python
 
+import argparse
+import os
+import importlib
+import time
+
 from jinja2 import Template
-from pysmt.shortcuts import And, get_model
-from register import X86StackRegister
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='guppypy synthesis lib')
+    parser.add_argument('-f', '--file', type=str,
+			help='synthesis template to use',
+			default='entry.S.synth', dest='synth_template')
+    parser.add_argument('-s', '--synth', type=str,
+			help='synthesizer to run (module.method)',
+			default='stack_order.synthesize', dest='synth')
+    parser.add_argument('-d', '--dest', type=str,
+			help='path in barrelfish to put synthesized code',
+			default='kernel/arch/x86_64/entry.S', dest='dest_path')
+    return parser.parse_args()
 
 
-def synthesize():
-    """
-    Use an SMT solver to synthesize the assembly for copying user-space stack
-    arguments into the kernel.
-    """     
+def commit_and_test(file_contents, dest_path, version):
 
-    # CPU registers
-    regs = X86StackRegister.get_stack_registers()
+    # chdir into barrelfish source repository
+    os.chdir('guppy')
 
-    # machine description
-    stack_ordering = And(
-        # every register has to be brought onto the stack
-        And(reg.has_valid_stack_slot() for reg in regs.values()),
+    # checkout a new branch
+    os.system('git checkout -b %s' % version)
 
-        # x86 syscall callframe register order
-        regs['%rcx'].precedes(regs['%r11']), 
-        regs['%r11'].precedes(regs['%rbx']),
-        regs['%rbx'].precedes(regs['%rbp']),
-        regs['%rbp'].precedes(regs['%rax']),
-        regs['%rax'].precedes(regs['%r15']),
-        regs['%rax'].precedes(regs['%r15']),
-        regs['%r15'].precedes(regs['%r14']),
-        regs['%r14'].precedes(regs['%r13']),
-        regs['%r13'].precedes(regs['%r12']),
-        regs['%r12'].precedes(regs['%r9']),
-        regs['%r9'].precedes(regs['%r8']),
-        regs['%r8'].precedes(regs['%r10']),
-    )
-        
-    # solve stack order
-    model = get_model(stack_ordering)
-    stack = sorted(regs.values(), key=lambda r: r.get_stack_index(model))
+    # put file in repo
+    with open(dest_path, 'w') as dest_file:
+	dest_file.write(file_contents)
 
-    print "\nSYNTHESIZED CODE>>>\n"
-    for reg in stack:
-        print reg.get_stack_index(model), reg.pushq()
+    # commit
+    os.system('git add %s' % dest_path)
+    os.system('git commit -m "[auto] add %s"' % version)
+
+    # build a docker image with the synthesized code
+    os.system('docker build . -t %s' % version)
+    os.system('docker run %s' % version)
+
+    # restore to trunk
+    os.system('git checkout dev')
+
+    # go back to pythonland
+    os.chdir('..')
+
+
+def main():
+    # load arguments from CLI
+    args = parse_args()
+
+    # name synthesizer version ('entry_S_synth_10298149812')
+    templ_esc = args.synth_template.replace('.', '_').lower()
+    synth_version = '%s_%s' % (templ_esc, int(time.time()))
+
+    # load synthesize() function from -s flag
+    mod, func = args.synth.rsplit('.', 1)
+    mod = importlib.import_module(mod)
+    synth_func = getattr(mod, func)
     
-    # generate code
-    asm = '\n'.join([reg.pushq() for reg in stack])
+    # load Jinja2 template in templates/ from -f flag
+    template = os.path.join('templates', args.synth_template)
+    t = Template(open(template, 'r').read())
 
-    return asm
+    # synthesize code
+    synth_output = t.render(synthesize=synth_func)
 
+    # commit synthesized code to barrelfish and test it on QEMU/x86
+    commit_and_test(synth_output, args.dest_path, synth_version)
 
-t = Template(open('entry.S.synth', 'r').read())
-print t.render(synthesize=synthesize)
+,
+if __name__ == '__main__':
+    main()
